6454eacafd6bdac50897d7e5fcf596cd3544215b
Laurie Gasc <laurie.gasc@hotmail.fr>
2026-02-04 16:23:56 +0100

    mise en place du scss pour un one page en scrollan les sections, squelette terminé

---
+++ eco-solaire-occitan/src/app/core/header/header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { AppRoutingModule } from "../../app.routes";
+
+@Component({
+  selector: 'app-header',
+  imports: [AppRoutingModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,23 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from "./core/footer/footer.component";
 import { HeaderComponent } from "./core/header/header.component";
+import { HomeComponent } from "./pages/home/home.component";
+import { ServicesComponent } from "./pages/services/services.component";
+import { AvisComponent } from "./pages/avis/avis.component";
+import { ContactComponent } from "./pages/contact/contact.component";
+import { RealisationsComponent } from "./pages/realisations/realisations.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    RouterOutlet,
     HeaderComponent,
     FooterComponent,
+    HomeComponent,
+    ServicesComponent,
+    AvisComponent,
+    ContactComponent,
+    RealisationsComponent
 ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
