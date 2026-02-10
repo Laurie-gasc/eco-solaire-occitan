a0b744be0f2651b2311aa1ea7f3f760d62ecfdf3
Laurie Gasc <laurie.gasc@hotmail.fr>
2026-02-10 13:31:59 +0100

    Mise en place des du click sur le header pour le scroll, ajout du design des liens header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,25 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
-import { RealisationsComponent } from './pages/realisations/realisations.component';
-import { ServicesComponent } from './pages/services/services.component';
-import { AvisComponent } from './pages/avis/avis.component';
-import { ContactComponent } from './pages/contact/contact.component';
-import { PresentationComponent } from './pages/presentation/presentation.component';
+
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'realisations', component: RealisationsComponent },
-  { path: 'presentation', component: PresentationComponent },
-  { path: 'services', component: ServicesComponent },
-  { path: 'avis', component: AvisComponent },
-  { path: 'contact', component: ContactComponent },
-  { path: '**', redirectTo: '' }
+  { path: '**', redirectTo: '' },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes), FormsModule, ReactiveFormsModule],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
