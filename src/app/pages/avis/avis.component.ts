ac1034ed80e8536e160981cfa469318b2c593c3d
Laurie Gasc <laurie.gasc@hotmail.fr>
2026-06-19 09:47:18 +0200

    Ajout des avis et meta description

--- src/app/pages/avis/avis.component.ts
+++ src/app/pages/avis/avis.component.ts
@@ -19,13 +19,13 @@ export class AvisComponent {
 
   avisList: Avis[] = [
      
-    // {
-    //   nom: '',
-    //   initiales: '',
-    //   note: ,
-    //   texte: '',
-    //   date: ''
-    // },
+     {
+      nom: 'Tim',
+       initiales: 'T',
+      note: 5 ,
+      texte: "Très satisfait de mon installation photovoltaïque de 3 kWc réalisée par Eco Solaire Occitan. L'équipe a été super réactive, à l’écoute et très professionnelle du début à la fin. Ils ont vraiment pris le temps de m’écouter, de répondre à toutes mes questions et de m’expliquer clairement les différentes étapes du projet. L’étude a été réalisée de manière très sérieuse, notamment grâce à une analyse professionnelle par drone, ce qui m’a permis d’avoir une vision précise et rassurante de l’installation. Le chantier s’est très bien déroulé, avec un travail propre, soigné et de très belles finitions.Je recommande vivement Eco Solaire Occitan pour leur sérieux, leur disponibilité et la qualité de leur travail.",
+       date: 'Nouveau'
+     },
   ];
 
   getEtoiles(note: number): number[] {
